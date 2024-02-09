43bb1be7aa54804a6fd68d21e5aca0568104eaf3
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2024-02-09 15:27:52 +0900

    従業員詳細画面作成

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -1,5 +1,38 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+@Service
+@Transactional
 public class EmployeeService {
-    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * 従業員情報を全件取得する.
+     * 
+     * @return 従業員情報
+     */
+    public List<Employee> showList() {
+        List<Employee> employee = employeeRepository.findAll();
+        return employee;
+    }
+
+    /**
+     * 従業員情報を取得する.
+     * 
+     * @param id
+     * @return 従業員情報
+     */
+    public Employee showDetail(Integer id) {
+        Employee employee = employeeRepository.load(id);
+        return employee;
+    }
 }

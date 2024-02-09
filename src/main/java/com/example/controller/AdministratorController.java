8cca72cc98a57b624fe0796893b6b56c0ac82f51
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2024-02-09 12:14:09 +0900

    管理者登録するメソッドの雛形を作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,16 +1,27 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+
 @Controller
 @RequestMapping("/administrator")
 public class AdministratorController {
+    @Autowired
+    private AdministratorService administratorService;
     
-    @GetMapping({"","/"})
-    public String index() {
-        System.out.println("test9");
+    /**
+     * 管理者登録機能.
+     * @param form
+     * @return 管理者登録画面
+     */
+    @GetMapping({"/toInsert","/toInsert/"})
+    public String toInsert(InsertAdministratorForm form) {
+        
         return "administrator/insert";
     }
 }
